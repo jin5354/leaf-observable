95d8b293416241b7e46237d82921fe0ef639da0d
jin5354 <xiaoyanjinx@gmail.com>
2017-06-21 21:22:41 +0800

    [chore] tidy configs

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Filename: index.js
+ * @Author: jin5354
+ * @Email: xiaoyanjinx@gmail.com
+ * @Last Modified time: 2017-06-21 21:05:06
+ */
+
+export * from './observable.js'
+export * from './watcher.js'
